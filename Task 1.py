a93a9a2c59419d9dfd7bf53106a6359796e56fb2
Satwik1229 <167329031+Satwik1229@users.noreply.github.com>
2025-07-26 16:38:35 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,16 @@
+a=input("enter the first value: ")
+b=input("enter the second value: ")
+a=float(a)
+b=float(b)
+c=a+b
+c=int(c)
+print("Addition: ",c)
+d=a-b
+d=int(d)
+print("Subtraction: ",d)
+E=a*b
+E=float(E)
+print("Multiplication: ",E)
+F=a/b
+F=float(F)
+print("Division: ",F)
---
+++ Task 2.py
@@ -0,0 +1,4 @@
+a=str(input("enter your first name: "))
+b=str(input("enter your second name: "))
+c=a+" "+b
+print("Hello," +c+"!" " welcome to the python program")
